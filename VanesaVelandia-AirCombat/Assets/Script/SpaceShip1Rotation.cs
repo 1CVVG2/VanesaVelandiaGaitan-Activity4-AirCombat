5dba4edee51a1cf497421431c6213d3f23fa673f
Vanesa Velandia <velandiavanesa@gmail.com>
2018-10-08 15:54:35 -0500

    Holaquehace

---
+++ VanesaVelandia-AirCombat/Assets/Script/SpaceShip1Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip1Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, Time.deltaTime*8, 0);
+
+
+    }
+}
